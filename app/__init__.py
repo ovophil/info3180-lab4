591d1db8e4a7b456cc6cd6f978ed98ca8a117edf
Philroy <philroy.williams@mymona.uwi.edu>
2018-02-24 06:02:02 +0000

    commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 # Config Values
 USERNAME = 'admin'
 PASSWORD = 'password123'
+ 
 
 # SECRET_KEY is needed for session security, the flash() method in this case stores the message in a session
 SECRET_KEY = 'Sup3r$3cretkey'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.config['UPLOAD_FOLDER'] = './app/static/uploads'
 from app import views
+
--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ import os
 from app import app
 from flask import render_template, request, redirect, url_for, flash, session, abort
 from werkzeug.utils import secure_filename
-
+from forms import UploadForm
 
 ###
 # Routing for your application.
@@ -32,15 +32,19 @@ def upload():
         abort(401)
 
     # Instantiate your form class
-
+    uploadForm = UploadForm()
+    
     # Validate file upload on submit
-    if request.method == 'POST':
-        # Get file data and save to your uploads folder
+    if request.method == 'POST' and uploadForm.validate_on_submit():
+        
+        upload = uploadForm.upload.data
+        filename = secure_filename(upload.filename)
+        upload.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
 
         flash('File Saved', 'success')
         return redirect(url_for('home'))
 
-    return render_template('upload.html')
+    return render_template('upload.html', uploadForm = uploadForm)
 
 
 @app.route('/login', methods=['POST', 'GET'])
